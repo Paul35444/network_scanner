76f2141abb81be1726880caa5353eb5bdbda2189
Paul St. Amour <paul35444@aol.com>
2021-02-17 12:46:42 -0600

    - added verbose=False to .srp to hide unnecessary info

--- network_scanner.py
+++ network_scanner.py
@@ -3,13 +3,13 @@
 import scapy.all as scapy
 
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip) #store IP in arp_request
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") #store Ether in broadcast
-    arp_request_broadcast = broadcast/arp_request #combine both
+    arp_request = scapy.ARP(pdst=ip)                    #store IP in arp_request
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")    #store Ether in broadcast
+    arp_request_broadcast = broadcast/arp_request       #combine both
 
     #.srp (send and recv) sends packets with cust Ether
     #[0] to only give us the first list which is answered packets
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0] #verbose=False hides everything but results
 
     for element in answered_list:
         print(element[1].psrc)  #psrc: source IP who sent packet
