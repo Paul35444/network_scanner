d294917d9b79ee6829083ca1d17892ce4c677b8a
Paul St. Amour <paul35444@aol.com>
2021-02-18 12:39:08 -0600

    - created print func - added for loop for results_list

--- network_scanner.py
+++ network_scanner.py
@@ -10,13 +10,18 @@ def scan(ip):
     #.srp (send and recv) sends packets with cust Ether
     #[0] to only give us the first list which is answered packets
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0] #verbose=False hides everything but results
-
-    print("IP\t\t\tMAC Address\n---------------------------------------------------------------------------------------") 
+ 
     clients_list = []
     for element in answered_list:
         client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc} 
         clients_list.append(client_dict)
     return(clients_list)
+
+def print_results(results_list):
+    print("IP\t\t\tMAC Address\n---------------------------------------------------------------------------------------")
+    for client in results_list:
+        print(client)
+
 
 #scan modem ip
 scan("192.168.1.1/24")
